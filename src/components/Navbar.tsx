c1354f55020d607ec6042bbb8c933f0d72e571b9
AV77web <villari.andrea@libero.it>
2026-05-25 12:09:56 +0200

    modificha di prova alla navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -10,7 +10,7 @@ export default function Navbar() {
         <>
             <div className="flex direction-row gap-7" >
                 <div className="text-3xl">Home</div>
-                <div className="text-3xl">About</div>
+                {/*<div className="text-3xl">About</div>*/}
                 <div className="text-3xl">Document</div>
                 <div className="text-3xl">Contact</div>
                 <div className="text-3xl">Sources</div>
